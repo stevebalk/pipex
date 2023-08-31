054424dd1f5ad4d61d72fbf9754d9ec2c2cbcb0a
steve <steve@pop-os.localdomain>
2023-08-31 17:34:58 +0200

    Getting empty envp to work

--- include/pipex.h
+++ include/pipex.h
@@ -6,7 +6,7 @@
 /*   By: sbalk <sbalk@student.fr>                   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/19 15:42:58 by sbalk             #+#    #+#             */
-/*   Updated: 2023/08/31 15:30:27 by sbalk            ###   ########.fr       */
+/*   Updated: 2023/08/31 17:25:24 by sbalk            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,48 +20,16 @@
 # include <string.h>
 # include <sys/wait.h>
 
-#define ERR_NOMEM	"Could not allocate memory!"
-#define ERR_CMD		"Command not found"
-
-
-typedef enum e_bool
-{
-	false,
-	true
-} t_bool;
-
 typedef struct s_pipex
 {
 	char	*prog_name;
 	int		in_fd;
 	int		out_fd;
-	t_bool	here_doc;
-	t_bool	is_invalid_infile;
 	char	*infile;
 	char	*outfile;
 	char	**env_paths;
 	char	***cmd_args;
-	int		cmd_count;
 	char	**envp;
 }			t_pipex;
-
-/* String functions */
-
-
-/* Error handling */
-
-
-/* index functions */
-
-
-/* Sorting functions */
-
-
-/* List functions */
-
-
-/* Push swap functions */
-
-
 
 #endif
