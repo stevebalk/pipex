53486f221802cd1b7f9527110c6e120d15d950d2
stevebalk <steve.balk@web.de>
2023-08-31 16:46:17 +0200

    Getting empty envp to work

--- src/pipex.c
+++ src/pipex.c
@@ -6,7 +6,7 @@
 /*   By: sbalk <sbalk@student.fr>                   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/16 13:58:52 by sbalk             #+#    #+#             */
-/*   Updated: 2023/08/31 16:44:18 by sbalk            ###   ########.fr       */
+/*   Updated: 2023/08/31 16:46:14 by sbalk            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -112,7 +112,7 @@ char	*get_command(t_pipex *pipex, int command)
 		free(ret);
 		i++;
 	}
-	ret = ft_strjoin(pipex->cmd_args[command][0], ": command not found`");
+	ret = ft_strjoin(pipex->cmd_args[command][0], ": command not found");
 	handle_error(pipex, ret, 0, 0);
 	free(ret);
 	return (NULL);
