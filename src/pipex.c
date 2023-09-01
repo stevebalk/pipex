549598b0a6ed79117bfc199cb23cb1ab9c5bbc49
stevebalk <steve.balk@web.de>
2023-09-01 18:38:47 +0200

    Working so far. ToDO cleaning

--- src/pipex.c
+++ src/pipex.c
@@ -6,7 +6,7 @@
 /*   By: sbalk <sbalk@student.fr>                   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/16 13:58:52 by sbalk             #+#    #+#             */
-/*   Updated: 2023/09/01 17:03:19 by sbalk            ###   ########.fr       */
+/*   Updated: 2023/09/01 18:27:22 by sbalk            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -45,36 +45,36 @@ void	handle_error(t_pipex *pipex, char *msg, int is_stderr, int shall_exit)
 	}
 }
 
-int	is_awk(char *str)
-{
-	while (ft_is_space(str))
-		str++;
-	if (ft_strncmp("awk ", str, 4) == 0)
-		return (1);
-	return (0);
-}
+// int	is_awk(char *str)
+// {
+// 	while (ft_is_space(str))
+// 		str++;
+// 	if (ft_strncmp("awk ", str, 4) == 0)
+// 		return (1);
+// 	return (0);
+// }
 
-char	**parse_awk(t_pipex *pipex, char *str)
-{
-	char	**ret;
+// char	**parse_awk(t_pipex *pipex, char *str)
+// {
+// 	char	**ret;
 
-	ret = ft_calloc(3, sizeof(char *));
-	if (ret == NULL)
-		handle_error(pipex, "calloc: parse awk: ", 1, 1);
-	ret[0] = ft_strdup("awk");
-	str = ft_strnstr(str, "awk", ft_strlen(str));
-	if (str != NULL)
-		str += 3;
-	while (ft_is_space(str))
-		str++;
-	if (*str == '\'')
-		ret[1] = ft_strtrim(str, "\'");
-	else
-		ret[1] = ft_strtrim(str, "\"");
-	if (ret[1] == NULL)
-		handle_error(pipex, "ft_strtrim: parse awk: ", 1, 1);
-	return (ret);
-}
+// 	ret = ft_calloc(3, sizeof(char *));
+// 	if (ret == NULL)
+// 		handle_error(pipex, "calloc: parse awk: ", 1, 1);
+// 	ret[0] = ft_strdup("awk");
+// 	str = ft_strnstr(str, "awk", ft_strlen(str));
+// 	if (str != NULL)
+// 		str += 3;
+// 	while (ft_is_space(str))
+// 		str++;
+// 	if (*str == '\'')
+// 		ret[1] = ft_strtrim(str, "\'");
+// 	else
+// 		ret[1] = ft_strtrim(str, "\"");
+// 	if (ret[1] == NULL)
+// 		handle_error(pipex, "ft_strtrim: parse awk: ", 1, 1);
+// 	return (ret);
+// }
 
 // int	has_quote(char *str)
 // {
@@ -218,9 +218,8 @@ char	*get_command(t_pipex *pipex, int command)
 	return (NULL);
 }
 
-void	parent(t_pipex *pipex, int fd[2], int pid)
+void	parent(t_pipex *pipex, int fd[2], int pid, char *command)
 {
-	char	*command;
 	int		stat_loc;
 
 	waitpid(pid, &stat_loc, WNOHANG);
@@ -228,7 +227,6 @@ void	parent(t_pipex *pipex, int fd[2], int pid)
 		exit(WEXITSTATUS(stat_loc));
 	if (dup2(fd[0], STDIN_FILENO) == -1)
 		handle_error(pipex, "dup2 (in): parent:", 1, 1);
-	// close(fd[0]);
 	pipex->out_fd = open(pipex->outfile, O_WRONLY | O_CREAT | O_TRUNC, 0644);
 	if (pipex->out_fd == -1)
 		handle_error(pipex, "could not create file:", 1, 1);
@@ -236,7 +234,6 @@ void	parent(t_pipex *pipex, int fd[2], int pid)
 		handle_error(pipex, "dup2 (out): parent:", 1, 1);
 	close(fd[1]);
 	close(pipex->out_fd);
-	command = get_command(pipex, 1);
 	if (command != NULL)
 	{
 		if (execve(command, pipex->cmd_args[1], pipex->envp) == -1)
@@ -245,14 +242,12 @@ void	parent(t_pipex *pipex, int fd[2], int pid)
 			handle_error(pipex, "parent: execve:", 1, 1);
 		}
 	}
-	else
-		exit(127);
+	free_pipex_struct(pipex);
+	exit(127);
 }
 
-void	child(t_pipex *pipex, int fd[2])
+void	child(t_pipex *pipex, int fd[2], char *command)
 {
-	char	*command;
-
 	if (dup2(fd[1], STDOUT_FILENO) == -1)
 		handle_error(pipex, "dup2 (out): child:", 1, 1);
 	// close(fd[1]);
@@ -263,7 +258,6 @@ void	child(t_pipex *pipex, int fd[2])
 		handle_error(pipex, "dup2 (in): child:", 1, 1);
 	close(fd[0]);
 	close(pipex->in_fd);
-	command = get_command(pipex, 0);
 	if (command != NULL)
 	{
 		if (execve(command, pipex->cmd_args[0], pipex->envp) == -1)
@@ -285,9 +279,9 @@ void	execute(t_pipex *pipex)
 	if (pid == -1)
 		handle_error(pipex, "execute: fork:", 1, 1);
 	if (pid == 0)
-		child(pipex, fd);
+		child(pipex, fd, get_command(pipex, 0));
 	else
-		parent(pipex, fd, pid);
+		parent(pipex, fd, pid, get_command(pipex, 1));
 }
 
 void	get_cmds(t_pipex *pipex, int argc, char **argv)
@@ -333,10 +327,12 @@ static void	add_slash_to_path(t_pipex *pipex)
 static void	get_env_paths(t_pipex *pipex, char **envp)
 {
 	int		i;
+	char	*path;
 
 	i = 0;
+	path = "PATH=/usr/local/bin:/usr/bin:/bin:/usr/local/sbin:/usr/sbin:/sbin";
 	if (envp[0] == NULL)
-		envp[0] = "PATH=/usr/local/bin:/usr/bin:/bin:/usr/local/sbin:/usr/sbin:/sbin";
+		envp[0] = path;
 	while (envp[i] != NULL)
 	{
 		if (ft_strncmp("PATH=", envp[i], 5) == 0)
@@ -351,24 +347,28 @@ static void	get_env_paths(t_pipex *pipex, char **envp)
 	}
 }
 
+void	intitalize_struct(int *argc, char **argv[], t_pipex *pipex, char *envp[])
+{
+	pipex->envp = envp;
+	pipex->prog_name = (*argv)[0];
+	pipex->prog_name += 2;
+	(*argc)--;
+	(*argv)++;
+	pipex->infile = (*argv)[0];
+	pipex->outfile = (*argv)[*argc - 1];
+}
+
 int	main(int argc, char *argv[], char *envp[])
 {
 	t_pipex	pipex;
 
-	pipex.envp = envp;
-	pipex.prog_name = argv[0];
-	pipex.prog_name += 2;
-	argc--;
-	argv++;
-	if (argc != 4)
+	if (argc != 5)
 	{
-		ft_putendl_fd("Not enough arguments (min 4)\n", STDERR_FILENO);
+		ft_putendl_fd("Wrong number of arguments:", STDERR_FILENO);
+		ft_putendl_fd("<infile> <cmd1> <cmd2> <outfile>", STDERR_FILENO);
 		return (EXIT_FAILURE);
 	}
-	else if (ft_streq("here_doc", argv[1]) && argc == 6)
-		return (1);
-	pipex.infile = argv[0];
-	pipex.outfile = argv[argc - 1];
+	intitalize_struct(&argc, &argv, &pipex, envp);
 	get_env_paths(&pipex, envp);
 	get_cmds(&pipex, argc, argv);
 	execute(&pipex);
