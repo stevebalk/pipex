53a9c86635a54b747b1d11ddd59be3259379d981
steve <steve@pop-os.localdomain>
2023-10-11 15:49:02 +0200

    still reworking child processes

--- src/execute.c
+++ src/execute.c
@@ -6,29 +6,29 @@
 /*   By: sbalk <sbalk@student.fr>                   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/08 13:48:32 by sbalk             #+#    #+#             */
-/*   Updated: 2023/10/10 13:04:21 by sbalk            ###   ########.fr       */
+/*   Updated: 2023/10/11 15:17:53 by sbalk            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex.h"
 
-static void	parent(t_pipex *pipex, int fd[2])
-{
-	if (dup2(fd[0], STDIN_FILENO) == -1)
-		handle_error(pipex, "dup2 (in): parent:", 1, 1);
-	pipex->out_fd = open(pipex->outfile, O_WRONLY | O_CREAT | O_TRUNC, 0644);
-	if (pipex->out_fd == -1)
-		handle_error(pipex, "could not create file:", 1, 1);
-	if (dup2(pipex->out_fd, STDOUT_FILENO) == -1)
-		handle_error(pipex, "dup2 (out): parent:", 1, 1);
-	close(fd[1]);
-	close(pipex->out_fd);
-	if (pipex->cmd_paths[1] != NULL)
-		if (execve(pipex->cmd_paths[1], pipex->cmd_args[1], pipex->envp) == -1)
-			handle_error(pipex, "parent: execve:", 1, 1);
-	free_pipex_struct(pipex);
-	exit(127);
-}
+// static void	parent(t_pipex *pipex, int fd[2])
+// {
+// 	if (dup2(fd[0], STDIN_FILENO) == -1)
+// 		handle_error(pipex, "dup2 (in): parent:", 1, 1);
+// 	pipex->out_fd = open(pipex->outfile, O_WRONLY | O_CREAT | O_TRUNC, 0644);
+// 	if (pipex->out_fd == -1)
+// 		handle_error(pipex, "could not create file:", 1, 1);
+// 	if (dup2(pipex->out_fd, STDOUT_FILENO) == -1)
+// 		handle_error(pipex, "dup2 (out): parent:", 1, 1);
+// 	close(fd[1]);
+// 	close(pipex->out_fd);
+// 	if (pipex->cmd_paths[1] != NULL)
+// 		if (execve(pipex->cmd_paths[1], pipex->cmd_args[1], pipex->envp) == -1)
+// 			handle_error(pipex, "parent: execve:", 1, 1);
+// 	free_pipex_struct(pipex);
+// 	exit(127);
+// }
 
 static void	child(t_pipex *pipex, int fd[2])
 {
@@ -47,26 +47,124 @@ static void	child(t_pipex *pipex, int fd[2])
 		handle_error(pipex, "child: execve", 1, 0);
 }
 
+// void	execute(t_pipex *pipex)
+// {
+// 	int		pid;
+// 	int		fd[2];
+// 	int		stat_loc;
+
+// 	stat_loc = 0;
+// 	if (pipe(fd) == -1)
+// 		handle_error(pipex, "execute: pipe:", 1, 1);
+// 	pid = fork();
+// 	if (pid == -1)
+// 		handle_error(pipex, "execute: fork:", 1, 1);
+// 	if (pid == 0)
+// 		child(pipex, fd);
+// 	if (pid > 0)
+// 	{
+// 		while (waitpid(pid, &stat_loc, 0) == 0)
+// 		{
+// 			if (WIFEXITED(stat_loc) && WEXITSTATUS(stat_loc) != 0)
+// 				exit(WEXITSTATUS(stat_loc));
+// 		}
+// 		parent(pipex, fd);
+// 	}
+// }
+
+
+// void	child(t_pipex *pipex, int fd[2])
+// {
+// 	if (pipex->cmd_paths[0] == NULL)
+// 		return ;
+// 	close(fd[0]);
+// 	if (dup2(fd[1], STDOUT_FILENO) == -1)
+// 		handle_error(pipex, "dup2 (out): child:", 1, 1);
+// 	if (pipex->in_fd == -1)
+// 		handle_error(pipex, "input:", 1, 1);
+// 	if (dup2(pipex->in_fd, STDIN_FILENO) == -1)
+// 		handle_error(pipex, "dup2 (in): child:", 1, 1);
+// 	close(pipex->in_fd);
+// 	if (execve(pipex->cmd_paths[0], pipex->cmd_args[0], pipex->envp) == -1)
+// 		handle_error(pipex, "child: execve", 1, 0);
+// }
+
+static void	child2(t_pipex *pipex, int fd[2])
+{
+	if (dup2(fd[0], STDIN_FILENO) == -1)
+		handle_error(pipex, "dup2 (in): parent:", 1, 1);
+	pipex->out_fd = open(pipex->outfile, O_WRONLY | O_CREAT | O_TRUNC, 0644);
+	if (pipex->out_fd == -1)
+		handle_error(pipex, "could not create file:", 1, 1);
+	if (dup2(pipex->out_fd, STDOUT_FILENO) == -1)
+		handle_error(pipex, "dup2 (out): parent:", 1, 1);
+	close(fd[1]);
+	close(pipex->out_fd);
+	if (pipex->cmd_paths[1] != NULL)
+		if (execve(pipex->cmd_paths[1], pipex->cmd_args[1], pipex->envp) == -1)
+			handle_error(pipex, "parent: execve:", 1, 1);
+	free_pipex_struct(pipex);
+	exit(127);
+}
+
 void	execute(t_pipex *pipex)
 {
-	int		pid;
-	int		fd[2];
-	// int		stat_loc;
+	int	pid;
+	int	fd[2];
+	// int	stat_loc;
+	// int exit_code = 0;
 
-	// stat_loc = 0;
 	if (pipe(fd) == -1)
 		handle_error(pipex, "execute: pipe:", 1, 1);
 	pid = fork();
 	if (pid == -1)
 		handle_error(pipex, "execute: fork:", 1, 1);
-	if (pid == 0)
-		child(pipex, fd);
-	if (pid > 0)
+	else if (pid == 0)
 	{
-		parent(pipex, fd);
+		child(pipex, fd);
 	}
-	// waitpid(pid, &stat_loc, 0);
-	// if (WIFEXITED(stat_loc) && WEXITSTATUS(stat_loc) != 0)
-	// 	exit(WEXITSTATUS(stat_loc));
-	// wait(0);
+	else if (pid > 0)
+	{
+		pid = fork();
+		if (pid == -1)
+			handle_error(pipex, "execute: fork:", 1, 1);
+		else if( pid == 0)
+			child2(pipex, fd);
+	}
+	// if (waitpid(pid, &stat_loc, 0) == 0)
+	// 	if (WIFEXITED(stat_loc))
+	// 		exit_code = WEXITSTATUS(stat_loc);
+	// wait(NULL);
+	exit(0);
 }
+
+
+// void	execute(t_pipex *pipex)
+// {
+// 	int		pid;
+// 	int		fd[2];
+// 	// int		stat_loc;
+
+// 	// stat_loc = 0;
+// 	if (pipe(fd) == -1)
+// 		handle_error(pipex, "execute: pipe:", 1, 1);
+// 	pid = fork();
+// 	if (pid == -1)
+// 		handle_error(pipex, "execute: fork:", 1, 1);
+// 	if (pid == 0)
+// 		child(pipex, fd);
+// 	else
+// 		close(fd[1]);
+// 	pid = fork();
+// 	if (pid == 0)
+// 	{
+// 		parent(pipex, fd);
+// 	}
+// 	else
+// 		close(fd[0]);
+// 	wait(NULL);
+// 	wait(NULL);
+// 	// waitpid(pid, &stat_loc, 0);
+// 	// if (WIFEXITED(stat_loc) && WEXITSTATUS(stat_loc) != 0)
+// 	// 	exit(WEXITSTATUS(stat_loc));
+// }
